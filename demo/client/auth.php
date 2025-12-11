446a06252fa4b0cd93bf4d8ca2285f4bd7f535b4
黄豆 <292951110@qq.com>
2025-12-11 16:55:59 +0800

    Update auth.php

--- demo/client/auth.php
+++ demo/client/auth.php
@@ -1,7 +1,7 @@
 <?php
 define('CLIENT_ID',     '应用ID');
 define('CLIENT_SECRET', '应用密钥');
-define('REDIRECT_URI',  '回调地址http://xxxx/client/callback');
+define('REDIRECT_URI',  urlencode('回调地址http://xxxx/client/callback'));
 define('OAUTH_SERVER',  '请求地址http://xxxx/server/index');
 $state = bin2hex(random_bytes(16));
 $_SESSION['oauth_state'] = $state;
